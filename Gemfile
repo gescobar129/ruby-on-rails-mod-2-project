32b533f065a91eaad7fa124d3c5583c64641e955
Gaida Escobar <gescobar129@gmail.com>
2019-10-21 20:22:33 -0400

    new changes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.1'
+require 'faker'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
@@ -60,3 +61,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "faker", "~> 2.6"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Article.destroy_all
+Comment.destroy_all
+
+5.times do 
+    User.create(username: Faker::Internet.username, password: "password", name: Faker::Name.name)
+end
+
+5.times do 
+    Article.create(title: Faker::Quote.most_interesting_man_in_the_world, content: Faker::Lorem.paragraphs)
+end
+
+5.times do 
+    Comment.create(user_id: User.ids.sample, article_id: Article.ids.sample)
+end
+
+
+
